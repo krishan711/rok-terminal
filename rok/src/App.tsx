09ed100b2b26a185209229449b1dc00e7a8bdeb6
Rikhil Raithatha <rikhilraithatha@Rikhis-MBP.localdomain>
2020-08-07 22:45:44 +0100

    Feature - add layout

--- rok/src/App.tsx
+++ rok/src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Row, Col } from 'antd';
+
+import SideBar from './sidebar';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Welcome to rok terminal
-        </p>
+        <Row>
+          <Col span={12}>{SideBar()}</Col>
+          <Col span={12}>Welcome to rok terminal</Col>
+        </Row>
       </header>
     </div>
   );

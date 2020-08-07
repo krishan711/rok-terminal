6022435f8823a7bb8ee00442eb562143d69fe368
Rikhil Raithatha <rikhilraithatha@Rikhis-MBP.localdomain>
2020-08-07 22:46:13 +0100

    .

---
+++ rok/src/sidebar.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { List, Button } from 'antd';
+
+
+const LIST_OF_COMMANDS = [
+  'moo',
+  'baa',
+  'oink',
+];
+
+function SideBar() {
+  const onButtonClick = (buttonText: string): void => {
+    console.log(buttonText)
+  }
+  return (
+    <div>
+      <List
+        bordered
+        dataSource={LIST_OF_COMMANDS}
+        renderItem={(item: string) => (
+          <List.Item>
+            <Button
+              onClick={() => onButtonClick(item)}
+              type="primary"
+            >
+              {item}
+            </Button>
+          </List.Item>
+        )}
+      />
+    </div>
+  )
+}
+
+export default SideBar;
